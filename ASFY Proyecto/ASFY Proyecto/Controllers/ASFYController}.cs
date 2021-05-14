291b16e6742270afe04ea2b8b47eb143689c2ff6
MBelenPiva <mbelenpiva@gmail.com>
2021-05-14 10:54:49 -0300

    Primer ejemplo ASFY

---
+++ ASFY Proyecto/ASFY Proyecto/Controllers/ASFYController}.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASFY_Proyecto.Controllers
+{
+    public class ASFYController : Controller
+    {
+        public ActionResult InicioSesion()
+        {
+            return View();
+        }
+
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        public ActionResult Programas()
+        {
+            return View();
+        }
+        public ActionResult MiPerfil()
+        {
+            return View();
+        }
+        public ActionResult SobreNosotras()
+        {
+            return View();
+        }
+    }
+}
