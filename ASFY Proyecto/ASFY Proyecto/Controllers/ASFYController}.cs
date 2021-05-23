2f7dbb276009ba2ab0e2bcfd73a5998798558d0d
MbelenPiva <mbelenpiva@gmail.com>
2021-05-23 11:48:07 -0300

    10 Version ASFY

--- ASFY Proyecto/ASFY Proyecto/Controllers/ASFYController}.cs
+++ ASFY Proyecto/ASFY Proyecto/Controllers/ASFYController}.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ASFY_Proyecto.Models;
 
 namespace ASFY_Proyecto.Controllers
 {
---
+++ ASFY Proyecto/ASFY Proyecto/Models/BDD.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASFY_Proyecto.Models
+{
+    public class BDD : Controller
+    {
+        // GET: BDD
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: BDD/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: BDD/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BDD/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BDD/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: BDD/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BDD/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: BDD/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
